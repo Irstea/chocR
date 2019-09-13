d90d43d196ea4abfb0a6d4a9188cd5536f4ee3af
hilaire drouineau <hilaire drouineau@irstea.fr>
2019-09-13 16:51:42 +0200

    correction fonction

--- R/plot_choc.R
+++ R/plot_choc.R
@@ -22,38 +22,47 @@
 #'
 #' @export
 
-plot_choc<-function(mychoc,ivar=c(1,2)){
+plot_choc <- function(mychoc, ivar = c(1, 2)) {
   overall_data <-
     do.call("rbind", lapply(mychoc$kernels, function(x)
       x$x))
-  mygrid<-mychoc$grid
-  if (ncol(overall_data)>2){ ###if we have more than 2 dimensions, we plot the diagram at the quantile of other dimensions
+  mygrid <- mychoc$grid
+  if (ncol(overall_data) > 2) {
+    ###if we have more than 2 dimensions, we plot the diagram at the quantile of other dimensions
     lev <- NA
-    apply(overall_data,2,function(x) {
-      lev<-unique(x)
-      lev[round(length(lev)/2)]
+    apply(overall_data, 2, function(x) {
+      lev <- unique(x)
+      lev[round(length(lev) / 2)]
     })
-    for (i in 1:ncol(mygrid)){
-      if (!i%in%ivar){
-        mygrid<-subset(mygrid,mygrid[,i]==lev[i])
+    for (i in 1:ncol(mygrid)) {
+      if (!i %in% ivar) {
+        mygrid <- subset(mygrid, mygrid[, i] == lev[i])
       }
     }
-    }
+  }
 
-  mychull <- chull(overall_data[,ivar])
-  mychull <- c(mychull,mychull[1])
+  mychull <- chull(overall_data[, ivar])
+  mychull <- c(mychull, mychull[1])
 
-  mygrid$inhull <- point.in.polygon(mygrid[,ivar[1]],mygrid[,ivar[2]],pol.x=overall_data[mychull,1],pol.y=overall_data[mychull,2])
-  mygrid$icolor<-ifelse(mygrid$tau>0,ifelse(mygrid$tau>mygrid$bsup,1,2),ifelse(mygrid$tau<mygrid$binf,4,3))
-  mygrid$icolor<-ifelse(mygrid$inhull,mygrid$icolor,0)
-  mygrid$icolor=as.factor(mygrid$icolor)
-  ggplot(mygrid,aes(x=mygrid[,ivar[1]],y=mygrid[,ivar[2]]))+
-    geom_raster(aes(fill=icolor))+
-    scale_fill_manual(values=c("white","green","palegreen1","pink1","red"),guide=FALSE)+
-    xlab(names(overall_data[,ivar[1]]))+
-    ylab(names(overall_data[,ivar[2]]))
+  mygrid$inhull <-
+    point.in.polygon(mygrid[, ivar[1]], mygrid[, ivar[2]], pol.x = overall_data[mychull, 1], pol.y =
+                       overall_data[mychull, 2])
+  mygrid$icolor <- NA
+  mygrid$icolor <-
+    ifelse(
+      mygrid$tau > 0,
+      ifelse(mygrid$tau > mygrid$bsup, 1, 2),
+      ifelse(mygrid$tau < mygrid$binf, 4, 3)
+    )
+  mygrid$icolor <- ifelse(mygrid$inhull, mygrid$icolor, 0)
+  mygrid$icolor <- as.factor(mygrid$icolor)
+  ggplot(mygrid, aes(x = mygrid[, ivar[1]], y = mygrid[, ivar[2]])) +
+    geom_raster(aes(fill = mychoc$icolor)) +
+    scale_fill_manual(
+      values = c("white", "green", "palegreen1", "pink1", "red"),
+      guide = FALSE
+    ) +
+    xlab(names(overall_data[, ivar[1]])) +
+    ylab(names(overall_data[, ivar[2]]))
 
 }
-
-
-
